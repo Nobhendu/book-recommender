5c0f40fcc748a892799aa090e591a3dc8627d558
Nobhendu <96724265+Nobhendu@users.noreply.github.com>
2025-06-03 22:22:03 +0530

    Add links to displayed books

--- app/src/components/Book.jsx
+++ app/src/components/Book.jsx
@@ -1,14 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Card from 'react-bootstrap/Card';
 import ListGroup from 'react-bootstrap/ListGroup';
 
 const Book = ({ book }) => {
   return (
     <Card className='my-3 p-3' style={{ width: '16rem' }}>
-      <Card.Img variant="top" src={book.coverImg} />
-      <Card.Body>
-        <Card.Title as='h6'>{book.title}</Card.Title>
-      </Card.Body>
+      <Link style={{color: 'inherit', textDecoration: 'inherit'}} to={"https://goodreads.com/book/show/"+book.bookId} target="_blank">
+      <div>
+        <Card.Img variant="top" src={book.coverImg} />
+        <Card.Body>
+          <Card.Title as='h6'>{book.title}</Card.Title>
+        </Card.Body>
+      </div>
+      </Link>
       <ListGroup className="list-group-flush">
         <ListGroup.Item style={{'fontSize': '0.9em'}}>Author: {book.author}</ListGroup.Item>
         <ListGroup.Item style={{'fontSize': '0.9em'}}>Rating: {book.rating}/5.0</ListGroup.Item>
